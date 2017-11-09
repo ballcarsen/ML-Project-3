917d0d9a67286f3c023a8cce3a4b37e3d2cf3728
Brett <Leasure57@gmail.com>
2017-11-09 10:42:31 -0700

    worked on Genetic Algorithm class

--- Project3/src/GeneticAlg.py
+++ Project3/src/GeneticAlg.py
@@ -1,2 +1,57 @@
 #Child class of EvoAlg, the Genetic Algorithm
 from src import EvoAlg
+import random
+
+class GeneticAlg(EvoAlg): 
+    
+    def train(self, maxIterations):
+        self.initPop()
+        while(self.genCount <= maxIterations):
+            self.currentGen += 1
+            parents = self.select()
+            children = []
+            # populate children via crossover
+            for i in range(len(parents)):
+                if (i % 2 == 0):
+                    if (i + 2 > len(parents)):
+                        children.append(parents[i])
+                    else:
+                        childArr = self.crossover(parents[i],parents[i+1])
+                        for child in childArr:
+                            children.append(child)
+            children = [self.gaussMuatate(child) | child in children]
+            self.population = children # generational replacement
+    
+    def gaussMuatate(self, child):
+        return child
+        
+    def select(self):
+        parents = []
+        for i in range(self.popSize):
+            parents.append(self.tournamentSelection(self.popSize/20))
+        return parents
+        
+    
+    def tournamentSelection(self, k):
+        competitors = [];
+        # randomly select k competitors
+        while(k > 0):
+            index = random.randint(0,self.popSize-1)
+            competitors.append(self.population[index])
+            k -= 1
+        # compete the competitors to get winner and return winner
+        winner = self.hostTournament(competitors)
+        return winner
+        
+    # find best competitor
+    def hostTournament(self, competitors):
+        best = competitors[0]
+        bestFitness = self.evalFitness(best)
+        for competitor in competitors:
+            if (self.evalFitness(competitor) > bestFitness):
+                best = competitor
+                bestFitness = self.evalFitness(best)
+        return best
+                
+           
+        
