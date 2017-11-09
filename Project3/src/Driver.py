afc8617df883de3cd2ef8baa503865e799ae6be1
Carsen Ball <ballcarsen@gmail.com>
2017-11-09 14:22:41 -0700

    10 fold cross

--- Project3/src/Driver.py
+++ Project3/src/Driver.py
@@ -1,10 +1,13 @@
 from src.shared.forwardProp import ForwardProp
+from src.Data import Data
 from pandas.tests.io.msgpack.test_read_size import test_correct_type_nested_array
 
 class Driver:
 
     def __init__(self):    
         self.numberOfClasses = 3 # will need to change this so it's not hard coded
+        self.data
+        self.expectedOut
 
     def test(self, input, expectedOut, network):
         correct = 0 #incorrect by default
@@ -13,13 +16,15 @@ class Driver:
         if (fp.getHypothesis() == expectedOut):
             correct = 1
         return correct
-
-    
-    def toArrayRep(self, index):
-        array = []
-        for i in range(self.numberOfClasses):
-            if (i == index):
-                array.append(1)
-            else:
-                array.append(0)
-        return array
+    #Accepts a file name, will return the data, and expeceted output in an array [data,out]
+#Sample data set
+#Pass number of classes
+dataSet1 = Data(7)
+#Pass file name
+dataSet1.readData('energy.txt')
+#do the foldin
+dataSet1.getFolds()
+#one set of data points, the first fold, we can train and test by iterating a loop over dataSet1.CrossValidatedTrain, etc
+#to get the 10 folds
+print(dataSet1.crossValidatedTrain[0])
+print(dataSet1.crossValidatedTrainOut[0])
